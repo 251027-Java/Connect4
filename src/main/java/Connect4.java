f578f46f26bab8f28bb0a4280c1cbbcfcaf2b2f6
snsorl <suryasolaiappan@gmail.com>
2025-10-30 15:54:10 -0500

    2D array of size 6 by 7

--- src/main/java/Connect4.java
+++ src/main/java/Connect4.java
@@ -3,5 +3,6 @@ public class Connect4 {
     public static void main(String [] args) {
         // new branch
         GameBoard.printGameBoard();
+        System.out.println("Choose X or O to play.");
     }
 }
--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 public class GameBoard {
 
     public static void printGameBoard() {
-        char[][] gameBoard = new char[4][4];
-        for(int i = 0; i < 4; i++) {
+        char[][] gameBoard = new char[6][7];
+        for(int i = 0; i < 6; i++) {
             Arrays.fill(gameBoard[i], '_');
         }
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i < 6; i++) {
             System.out.println(Arrays.toString(gameBoard[i]));
         }
     }
