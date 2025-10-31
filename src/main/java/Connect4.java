e2223d3ba3c934db9b9de8b1602bfdbe12b41d88
snsorl <suryasolaiappan@gmail.com>
2025-10-30 19:56:53 -0500

    added comments

--- src/main/java/Connect4.java
+++ src/main/java/Connect4.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Connect4 {
     public static void main(String [] args) {
-        // new branch
-        int gameTurns = 5;
+        // Game ends when the method in GameBoard.java to check for four is implemented or when 42 X's and O's total are inserted.
+        int gameTurns = 42;
         GameBoard.initializeGameBoard();
         GameBoard.printGameBoard();
+
         while(gameTurns>=0){
             GameBoard.playGame();
             GameBoard.lowestRowToInsert();
--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,43 +1,52 @@
-import java.util.Arrays;
-import java.util.Scanner;
+    import java.util.Arrays;
+    import java.util.Scanner;
 
-public class GameBoard {
-    static char[][] gameBoard = new char[6][7];
-    static int columnNumber;
-    static char playerType;
-    public static void initializeGameBoard() {
-        for(int i = 0; i < 6; i++) {
-            Arrays.fill(gameBoard[i], '_');
-        }
-
-    }
-
-    public static void printGameBoard() {
-        for(int i = 0; i < 6; i++) {
-            System.out.println(Arrays.toString(gameBoard[i]));
-        }
-
-    }
-
-    public static void playGame() {
-        System.out.println("Choose X or O to play: ");
-        Scanner inputReader = new Scanner(System.in);
-         playerType = inputReader.next().charAt(0);
-        System.out.println("Choose a column to insert: 1, 2, 3, 4, 5, 6, 7 ");
-        columnNumber = inputReader.nextInt();
-    }
-
-    public static int lowestRowToInsert(){
-        int i ;
-        boolean inserted = false;
-        for(i = 5; i >= 0; i--) {
-            if(gameBoard[i][columnNumber-1] == '_' && !inserted){
-                gameBoard[i][columnNumber-1] = playerType;
-                inserted = true;
+    public class GameBoard {
+        // create and initialize gameboard
+        static char[][] gameBoard = new char[6][7];
+        static int columnNumber;
+        static char playerType;
+        public static void initializeGameBoard() {
+            for(int i = 0; i < 6; i++) {
+                Arrays.fill(gameBoard[i], '_');
             }
-        }
-        printGameBoard();
-        return i+1;
-    }
 
-}
+        }
+
+        public static void printGameBoard() {
+            for(int i = 0; i < 6; i++) {
+                System.out.println(Arrays.toString(gameBoard[i]));
+            }
+
+        }
+
+        public static void playGame() {
+            // Add player 2 and don't ask for this every time later.
+            System.out.println("Choose X or O to play: ");
+            Scanner inputReader = new Scanner(System.in);
+             playerType = inputReader.next().charAt(0);
+            System.out.println("Choose a column to insert: 1, 2, 3, 4, 5, 6, 7 ");
+            columnNumber = inputReader.nextInt();
+
+        }
+
+        public static void lowestRowToInsert(){
+            // figure out which row to insert X or O based on user's column choice
+            int i ;
+            boolean inserted = false;
+            for(i = 5; i >= 0; i--) {
+                if(gameBoard[i][columnNumber-1] == '_' && !inserted){
+                    gameBoard[i][columnNumber-1] = playerType;
+                    inserted = true;
+                }
+            }
+            printGameBoard();
+        }
+
+        /*
+        ADD method to check if four in a row/column or diagonal.
+
+
+
+         */
+    }
